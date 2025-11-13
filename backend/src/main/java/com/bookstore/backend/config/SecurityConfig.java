f936c6678351c67dc2860a2f8bdb51f35e813fee
hoangphuonglxag <phuongdinhhoang18@gmail.com>
2025-11-13 14:48:41 +0700

    chore:Folder(BE, FE, DB)

--- backend/src/main/java/com/bookstore/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/bookstore/backend/config/SecurityConfig.java
@@ -3,8 +3,8 @@ package com.bookstore.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
@@ -14,11 +14,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/users/**").permitAll()
-                .anyRequest().authenticated()
-            );
-        
+            .authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
+
         return http.build();
     }
 }
