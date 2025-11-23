d22173e3b554c391c0d0006006762294eca101f9
tmai1106 <23133071@student.hcmute.edu.vn>
2025-11-24 06:58:19 +0700

    feat: add routing for voucher page
    - Install react-router-dom for routing management
    - Create Home page as landing page
    - Move VoucherManagement from components to pages folder
    - Add routing in main.jsx (/ for Home, /vouchers for Voucher Management)
    - Update Header with Link component and add Voucher navigation icon
    - Add responsive Home page with hero section and feature cards

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './main.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import VoucherManagement from './components/VoucherManagement/VoucherManagement'
+import Home from './pages/Home'
+import VoucherManagement from './pages/VoucherManagement/VoucherManagement'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header />
-    <VoucherManagement />
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/vouchers" element={<VoucherManagement />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </StrictMode>,
 )
