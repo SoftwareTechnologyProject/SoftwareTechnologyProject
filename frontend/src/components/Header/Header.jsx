a1fb9c5b7b087cb27c8c7bae2621b24c1c95b9f5
Đức Thịnh <thinh17qn@gmail.com>
2025-11-25 14:22:11 +0700

    style: Add basic UI Account

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,56 +1,65 @@
-import React from "react"
+import React from "react";
 import bannerHeader from '../../assets/banner/banner-header.png';
 import logo from '../../assets/logo/logo.png';
-import "./Header.css"
+import "./Header.css";
 import { BsGrid } from "react-icons/bs";
 import { CiSearch } from "react-icons/ci";
 import { FiShoppingCart } from "react-icons/fi";
 import { GoBell } from "react-icons/go";
 import { CiUser } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <>
-            <div className="bg-[var(--components-color)]">
-                <header>
-                    <img src={bannerHeader} alt="banner" />
-                </header>
+        <div className="bg-[var(--components-color)]">
+            <header>
+                <img src={bannerHeader} alt="banner" />
+            </header>
 
-                <nav>
-                    <div className="nav-left">
+            <nav>
+                <div className="nav-left">
+                    <Link to="/">
                         <img src={logo} alt="logo" />
+                    </Link>
+                </div>
+
+                <div className="nav-search">
+                    <input type="text" placeholder="Tìm kiếm..." className="input-search"/>
+                    <button><CiSearch className="w-5 h-5 cursor-pointer" /></button>
+                </div>
+
+                <div className="nav-right">
+                    <div>
+                        <Link to="/category" className="flex flex-col items-center">
+                            <BsGrid className="nav-icons" />
+                        </Link>
+                        <span>Danh mục</span>
                     </div>
 
-                    <div className="nav-search">
-                        <input type="text" placeholder="Tìm kiếm..." className="input-search"/>
-                        <button><CiSearch className="w-5 h-5 cursor-pointer" /></button>
+                    <div className="mx-4">
+                        <Link to="/notifications" className="flex flex-col items-center">
+                            <GoBell className="nav-icons" />
+                        </Link>
+                        <span>Thông báo</span>
                     </div>
 
-                    <div className="nav-right">
-                        <div>
-                            <a href="#" className="flex flex-col items-center"><BsGrid className="nav-icons" /></a>
-                            <span>Danh mục</span>
-                        </div>
-
-                        <div className="mx-4">
-                            <a href="#" className="flex flex-col items-center"><GoBell className="nav-icons" /></a>
-                            <span>Thông báo</span>
-                        </div>
-
-                        <div>
-                            <a href="#" className="flex flex-col items-center"><FiShoppingCart className="nav-icons" /></a>
-                            <span>Giỏ hàng</span>
-                        </div>
-
-                        <div className="mx-4">
-                            <a href="#" className="flex flex-col items-center"><CiUser className="nav-icons" /></a>
-                            <span>Tài khoản</span>
-                        </div>
+                    <div>
+                        <Link to="/cart" className="flex flex-col items-center">
+                            <FiShoppingCart className="nav-icons" />
+                        </Link>
+                        <span>Giỏ hàng</span>
                     </div>
-                </nav>
-            </div>
-        </>
-    )
-}
 
-export default Header
+                    <div className="mx-4">
+                        <Link to="/account" className="flex flex-col items-center">
+                            <CiUser className="nav-icons" />
+                        </Link>
+                        <span>Tài khoản</span>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
