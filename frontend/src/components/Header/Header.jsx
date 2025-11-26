8ffa2e01ee33d1f7c2deac7d6de581aba054c991
hoangphuonglxag <phuongdinhhoang18@gmail.com>
2025-11-26 12:02:45 +0700

    fix: sửa lại application.yaml và docker-compose.yml
    - Loại bỏ conflict markers trong application.yaml
    - Cập nhật cấu hình đúng cho PostgreSQL port 5433
    - Giữ nguyên docker-compose.yml theo yêu cầu
    - Fix Header.jsx conflicts

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,25 @@
+<<<<<<< HEAD
 import React from "react";
-import { Link } from "react-router-dom";
 import bannerHeader from '../../assets/banner/banner-header.png';
 import logo from '../../assets/logo/logo.png';
 import "./Header.css";
+=======
+import React from "react"
+import { Link } from "react-router-dom"
+import bannerHeader from '../../assets/banner-header.png';
+import logo from '../../assets/logo.png';
+import "./Header.css"
+>>>>>>> origin/feature/voucher-crud
 import { BsGrid } from "react-icons/bs";
 import { CiSearch } from "react-icons/ci";
 import { FiShoppingCart } from "react-icons/fi";
 import { GoBell } from "react-icons/go";
 import { CiUser } from "react-icons/ci";
+<<<<<<< HEAD
+import { Link } from "react-router-dom";
+=======
 import { RiCoupon3Line } from "react-icons/ri";
+>>>>>>> origin/feature/voucher-crud
 
 const Header = () => {
     return (
@@ -17,6 +28,7 @@ const Header = () => {
                 <img src={bannerHeader} alt="banner" />
             </header>
 
+<<<<<<< HEAD
             <nav>
                 <div className="nav-left">
                     <Link to="/">
@@ -35,13 +47,13 @@ const Header = () => {
                             <BsGrid className="nav-icons" />
                         </Link>
                         <span>Danh mục</span>
-                    </div>
-
-                    <div className="mx-4">
-                        <Link to="/vouchers" className="flex flex-col items-center">
-                            <RiCoupon3Line className="nav-icons" />
+=======
+                <nav>
+                    <div className="nav-left">
+                        <Link to="/">
+                            <img src={logo} alt="logo" />
                         </Link>
-                        <span>Voucher</span>
+>>>>>>> origin/feature/voucher-crud
                     </div>
 
                     <div className="mx-4">
@@ -51,11 +63,39 @@ const Header = () => {
                         <span>Thông báo</span>
                     </div>
 
+<<<<<<< HEAD
                     <div>
                         <Link to="/cart" className="flex flex-col items-center">
                             <FiShoppingCart className="nav-icons" />
                         </Link>
                         <span>Giỏ hàng</span>
+=======
+                    <div className="nav-right">
+                        <div>
+                            <a href="#" className="flex flex-col items-center"><BsGrid className="nav-icons" /></a>
+                            <span>Danh mục</span>
+                        </div>
+
+                        <div className="mx-4">
+                            <Link to="/vouchers" className="flex flex-col items-center"><RiCoupon3Line className="nav-icons" /></Link>
+                            <span>Voucher</span>
+                        </div>
+
+                        <div className="mx-4">
+                            <a href="#" className="flex flex-col items-center"><GoBell className="nav-icons" /></a>
+                            <span>Thông báo</span>
+                        </div>
+
+                        <div>
+                            <a href="#" className="flex flex-col items-center"><FiShoppingCart className="nav-icons" /></a>
+                            <span>Giỏ hàng</span>
+                        </div>
+
+                        <div className="mx-4">
+                            <a href="#" className="flex flex-col items-center"><CiUser className="nav-icons" /></a>
+                            <span>Tài khoản</span>
+                        </div>
+>>>>>>> origin/feature/voucher-crud
                     </div>
 
                     <div className="mx-4">
