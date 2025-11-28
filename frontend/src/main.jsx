03cf1145481a70ddb47a4578855ef1925b8382db
Đức Thịnh <thinh17qn@gmail.com>
2025-11-28 13:27:43 +0700

    style: fix code UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './main.css'
 import HomePage from "./pages/HomePage/HomePage";
 import Account from "./pages/Account/Account";
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/account" element={<Account />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   </StrictMode>
 );
