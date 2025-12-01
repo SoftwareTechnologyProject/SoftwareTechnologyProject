4bcbf00551f910647cd6eeac62a79194004a402b
NguyenHuuTam <huutamand@gmail.com>
2025-12-02 00:07:34 +0700

    frontend order

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import ProductDetail from './pages/productDetails/ProductDetail';
 import Account from './pages/Account/Account';
+import Order from './pages/Order/Order';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/books/:id" element={<ProductDetail />} />
           <Route path="/account" element={<Account />} />
+          <Route path="/order" element={<Order />} />      {/* Order */}
         </Routes>
       </div>
     </Router>
