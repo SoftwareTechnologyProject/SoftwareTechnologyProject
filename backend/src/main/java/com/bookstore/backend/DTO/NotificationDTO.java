23bf0347c462e92aa33ac79c910614e718e1de94
huyluong <luongngochuy1903@gmail.com>
2025-12-01 09:29:16 +0700

    feature: notification (broadcast) FE + BE (dang can userID de test them)

--- backend/src/main/java/com/bookstore/backend/DTO/NotificationDTO.java
+++ backend/src/main/java/com/bookstore/backend/DTO/NotificationDTO.java
@@ -4,11 +4,13 @@ import com.bookstore.backend.model.Notification;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class NotificationDTO {
     private String content;
--- backend/src/main/java/com/bookstore/backend/service/NotificationService.java
+++ backend/src/main/java/com/bookstore/backend/service/NotificationService.java
@@ -3,17 +3,35 @@ package com.bookstore.backend.service;
 import com.bookstore.backend.DTO.NotificationDTO;
 import com.bookstore.backend.model.Notification;
 import com.bookstore.backend.model.Users;
+import com.bookstore.backend.repository.NotificationRepository;
 import com.bookstore.backend.utils.SecurityUtils;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
 @Service
 public class NotificationService {
     private final SimpMessagingTemplate messagingTemplate;
+    private final NotificationRepository notificationRepository;
+
+    public List<NotificationDTO> getAllNotification(int page, int size){
+        var user = SecurityUtils.getCurrentUser();
+        if (user == null){
+            return null;
+        }
+        List<Notification> result = notificationRepository.findByUsers_IdOrderByCreateAtDesc(user.getId(), PageRequest.of(page, size)).getContent();
+        return result.stream()
+                .map(noti -> NotificationDTO.from(noti))
+                .collect(Collectors.toList());
+    }
 
     public void broadcastNotification(Notification notification){
         NotificationDTO notificationDTO = NotificationDTO.from(notification);
