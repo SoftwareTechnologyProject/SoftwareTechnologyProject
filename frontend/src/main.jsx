91a1f0bb4be34dd695a6fa3eb2e513761636386f
hoangphuonglxag <phuongdinhhoang18@gmail.com>
2025-12-10 12:57:53 +0700

    update: Merge danh muc product, layout homepage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,38 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './main.css';
+
+// Components
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import AccountLayout from './components/AccountLayout/AccountLayout.jsx';
-import HomePage from "./pages/HomePage/HomePage";
-import Account from "./pages/Account/Account.jsx";
-import ProductDetail from "./pages/productDetails/ProductDetail";
-import VoucherManagement from './pages/VoucherManagement/VoucherManagement.jsx';
 import Recommend from './components/Recommend/Recommend.jsx';
+import HeaderAdmin from './components/HeaderAdmin/HeaderAdmin.jsx';
+import AccountLayout from './components/AccountLayout/AccountLayout.jsx';
+
+// Pages
+import HomePage from './pages/HomePage/HomePage';
+import Account from './pages/Account/Account.jsx';
+import ProductDetail from './pages/Book/ProductDetail.jsx';
+import VoucherManagement from './pages/VoucherManagement/VoucherManagement.jsx';
 import VoucherWallet from './pages/VoucherWallet/VoucherWallet';
 import Order from './pages/Order/Order';
 import OrderAdmin from './pages/OrderAdmin/OrderAdmin';
 import OrderDetail from './pages/OrderDetail/OrderDetail';
-import HeaderAdmin from "./components/HeaderAdmin/HeaderAdmin.jsx";
+import CategoryPage from './pages/Category/CategoryPage';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Header />
-
       <Routes>
+        {/* Public */}
         <Route path="/" element={<HomePage />} />
+        <Route path="/:categorySlug" element={<CategoryPage />} />
+        <Route path="/books/:id" element={<ProductDetail />} />
+        <Route path="/vouchers" element={<VoucherManagement />} />
+
+        {/* Account area */}
         <Route path="/account" element={<AccountLayout />}>
           <Route path="accountInf" element={<Account />} />
           <Route path="voucher-wallet" element={<VoucherWallet />} />
@@ -31,16 +41,14 @@ createRoot(document.getElementById("root")).render(
           <Route path="order/:id" element={<OrderDetail />} />
         </Route>
 
-        {/* Admin */}
+        {/* Admin (placeholder) */}
         <Route path="/admin" element={<HeaderAdmin />} >
           <Route path="books" element={<Account />} />
         </Route>
-        <Route path="/books/:id" element={<ProductDetail />} />
-        
       </Routes>
 
       <Recommend />
       <Footer />
     </BrowserRouter>
   </StrictMode>
-);
+)
